2693019fc77243361187e2e60d690aeaaa9e8090
jessica qiu <jessica.qiu010405@gmail.com>
2024-12-28 16:20:34 +1100

    Initial Commit

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Hero() {
+  return (
+        <section className="bg-background flex items-center flex-col">
+   <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex">
+    <div className="mx-auto max-w-xl text-center">
+      <h1 className="text-3xl font-extrabold sm:text-5xl">
+       Manage your Expenses
+        <strong className="font-extrabold text-primary sm:block"> Control your Money </strong>
+      </h1>
+
+      <p className="mt-4 sm:text-xl/relaxed">
+        Take control of your spending today, and watch your future self thank you tomorrow!
+      </p>
+
+      <div className="mt-8 flex flex-wrap justify-center gap-4">
+        <a
+          className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-secondary focus:outline-none focus:ring active:bg-primry sm:w-auto"
+          href="/sign-in"
+        >
+          Get Started
+        </a>
+
+        
+      </div>
+    </div>
+  </div>
+  <Image src={'/dashboard.png'} alt='dashboard'
+  width={1000}
+  height={700}
+  className='mt-9 rounded-xl border-2'
+  />
+</section>
+  )
+}
+
+export default Hero
